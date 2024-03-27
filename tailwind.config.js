24833de0348de062219bc0774dd9891d57bdec6f
ahmed-sudani <ahmed.sudani570@gmail.com>
2024-03-27 09:31:02 +0200

    fix: remove rotate animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      animation: {
-        rotate: "rotate 12s linear infinite",
-      },
-      keyframes: {
-        rotate: {
-          from: {
-            "background-position": "0px 0px;",
-          },
-          to: {
-            "background-position": "610px 0px;",
-          },
-        },
-      },
       fontFamily: {
         serif: ['"Bellefair"', ...defaultTheme.fontFamily.serif],
         sans: ['"Barlow Condensed"', ...defaultTheme.fontFamily.sans],
